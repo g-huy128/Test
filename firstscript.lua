430eecb0cb524845c314cbaa37a0f0e74fcb426d
Huy <159507316+Ghuy2008@users.noreply.github.com>
2026-01-30 14:06:50 +0700

    Add files via upload

---
+++ firstscript.lua
@@ -0,0 +1,52 @@
+while true do
+    task.wait(0.1)
+    local args = {
+	buffer.fromstring("5"),
+	buffer.fromstring("\254\001\000\006\tBasicDice")
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Shared"):WaitForChild("Warp"):WaitForChild("Index"):WaitForChild("Event"):WaitForChild("Reliable"):FireServer(unpack(args))
+local args = {
+	buffer.fromstring("-"),
+	buffer.fromstring("\254\001\000\006\aPodium1")
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Shared"):WaitForChild("Warp"):WaitForChild("Index"):WaitForChild("Event"):WaitForChild("Reliable"):FireServer(unpack(args))
+local args = {
+	buffer.fromstring("-"),
+	buffer.fromstring("\254\001\000\006\aPodium2")
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Shared"):WaitForChild("Warp"):WaitForChild("Index"):WaitForChild("Event"):WaitForChild("Reliable"):FireServer(unpack(args))
+local args = {
+	buffer.fromstring("-"),
+	buffer.fromstring("\254\001\000\006\aPodium3")
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Shared"):WaitForChild("Warp"):WaitForChild("Index"):WaitForChild("Event"):WaitForChild("Reliable"):FireServer(unpack(args))
+local args = {
+	buffer.fromstring("-"),
+	buffer.fromstring("\254\001\000\006\aPodium4")
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Shared"):WaitForChild("Warp"):WaitForChild("Index"):WaitForChild("Event"):WaitForChild("Reliable"):FireServer(unpack(args))
+local args = {
+	buffer.fromstring("-"),
+	buffer.fromstring("\254\001\000\006\aPodium5")
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Shared"):WaitForChild("Warp"):WaitForChild("Index"):WaitForChild("Event"):WaitForChild("Reliable"):FireServer(unpack(args))
+local args = {
+	buffer.fromstring("\f"),
+	buffer.fromstring("\254\001\000\006\tBasicDice")
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Shared"):WaitForChild("Warp"):WaitForChild("Index"):WaitForChild("Event"):WaitForChild("Reliable"):FireServer(unpack(args))
+local args = {
+	buffer.fromstring("-"),
+	buffer.fromstring("\254\001\000\006\aPodium6")
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Shared"):WaitForChild("Warp"):WaitForChild("Index"):WaitForChild("Event"):WaitForChild("Reliable"):FireServer(unpack(args))
+
+local args = {
+	buffer.fromstring("-"),
+	buffer.fromstring("\254\001\000\006\aPodium7")
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Shared"):WaitForChild("Warp"):WaitForChild("Index"):WaitForChild("Event"):WaitForChild("Reliable"):FireServer(unpack(args))
+
+
+
+end
