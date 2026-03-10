f7b599430ec8d656e24fcc5e5fe7cbde33103c6e
Huy <159507316+g-huy128@users.noreply.github.com>
2026-03-10 20:33:14 +0700

    Add files via upload

---
+++ Test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+clear
+echo "=====MENU====="
+read -p "Enter your name: " name
+read -sp "Enter your password: " password
+read -p "Enter your age: " age
+echo "Your name is $name"
+echo "Your password is $password"
+echo "Your age is $age"
